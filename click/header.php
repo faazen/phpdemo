3891e0ff39655ec4018a1a67109f70c20aebc84d
faazen <97652528+faazen@users.noreply.github.com>
2022-06-03 18:31:52 +0530

    Update header.php

--- click/header.php
+++ click/header.php
@@ -12,21 +12,21 @@
  Visit website : www.mayurik.com -->  
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
-  <link href="assets/DataTables/datatables.min.css" rel="stylesheet">
-  <link href="assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
-  <link href="assets/css/select2.min.css" rel="stylesheet">
+  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+  <link href="/assets/DataTables/datatables.min.css" rel="stylesheet">
+  <link href="/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
+  <link href="/assets/css/select2.min.css" rel="stylesheet">
 
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-  <link type="text/css" rel="stylesheet" href="assets/css/jquery-te-1.4.0.css">
+  <link href="/assets/css/style.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="/assets/css/jquery-te-1.4.0.css">
   
   
 <?php include('footer.php');?>
